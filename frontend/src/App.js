f7577aed8f77fc519c0785761ffe9cd273d221f4
DiegoVillelaCano <46820606+DiegoVillelaCano@users.noreply.github.com>
2022-01-17 18:53:05 -0600

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>{"#Estudiantes"}</h3>
+      <h3>{"#Cursos"}</h3>
+      <h3>{"#catedraticos"}</h3>
+      <h3>{"#Asignasiones"}</h3>
+    </div>
+  );
+}
+
+export default App;
